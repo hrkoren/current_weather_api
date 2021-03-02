d38d460c8a798595aa338ae377d75d9a1f0ee1eb
HRush <hrkoren@gmail.com>
2021-03-02 18:25:13 -0500

    made more changes to code in html, js, and css

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ let cityContainerEl = $('.cityContainer');
 let cityNameEl = $('.cityName');
 let searchBtn = $('#Search_button'); 
 let cityListEl = $('#city-list');
+const apiKey = 'a4fdb9683d6a1f4cdbbf60857995908b';
 
 function handleFormSubmit(event) {
   event.preventDefault();
@@ -31,9 +32,9 @@ var printCities = function (name) {
 }
 
 function cityWeather(cityNameEl) {
-  //fetch with cityNameEl causes an error
-    // fetch('http://api.openweathermap.org/data/2.5/weather?q=' + cityNameEl + '&appid=a4fdb9683d6a1f4cdbbf60857995908b') 
-     fetch('http://api.openweathermap.org/data/2.5/weather?q=Pittsburgh&appid=a4fdb9683d6a1f4cdbbf60857995908b')   
+  
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=Pittsburgh&appid=a4fdb9683d6a1f4cdbbf60857995908b')   
+  // fetch('http://api.openweathermap.org/data/2.5/weather?q=' + $(cityNameEl) + '&appid=' + $(apiKey))   
   .then(function(resp) { 
     return resp.json() 
   })
@@ -49,12 +50,24 @@ window.onload = function() {
   cityWeather();
 }
 
+
+
+
+
 function getWeather(weatherData) {
 	var fahrenheit = Math.round(((parseFloat(weatherData.main.temp)-273.15)*1.8)+32); 
+  // var weatherIcon = document.querySelector('#icon');
+  // const {icon} = data.weather[0];
+  // var iconUrl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 	
 	document.getElementById('description').innerHTML = weatherData.weather[0].description;
+  // document.getElementById('weatherIcon').innerHTML = weatherIcon(iconUrl);
 	document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
 	document.getElementById('location').innerHTML = weatherData.name;
+  document.getElementById('wind').innerHTML += weatherData.wind.speed;
+  document.getElementById('humidity').innerHTML += weatherData.main.humidity;
+  // document.getElementById('uv').innerHTML = weatherData.uvi; 
+  // not seeing UV index in console but it's supposed to be uvi
 }
 
 
