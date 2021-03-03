c07b66b72b97b1d8c821e777b650b9965744be88
HRush <hrkoren@gmail.com>
2021-03-03 18:20:49 -0500

    fixed script code for api and search

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 let searchFormEl = $('#search-form');
-let cityContainerEl = $('.cityContainer');
+let cityInputEl = $('#city-search');
+let cityContainerEl = $('#cityContainer');
+let citySearchName = $('#city-search-name');
 let cityNameEl = $('.cityName');
-let searchBtn = $('#Search_button'); 
+let searchBtn = $('.btn'); 
 let cityListEl = $('#city-list');
 const apiKey = 'a4fdb9683d6a1f4cdbbf60857995908b';
 
@@ -13,28 +15,17 @@ function handleFormSubmit(event) {
     alert('Please enter a city');
     return;
   }
-  cityContainerEl.replaceWith('<h4>' + searchInputEl + '</h4>');
-
-  $('input[name="city-search"]').val('');
-  // console.log(searchInputEl);
+  cityWeather(searchInputEl);
 }
 
 searchFormEl.on('submit', handleFormSubmit);
-//NOTE: icon display next to city name that matches weather
 
 
-//need to append to List of Searched Cities - not working!!
-var printCities = function (name) {
-  var listEl = $('<li>');
-  var listDetail = name;
-  listEl.addClass('list-group-item').text(listDetail);
-  listEl.appendTo(cityListEl);
-}
 
 function cityWeather(cityNameEl) {
   
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=Pittsburgh&appid=a4fdb9683d6a1f4cdbbf60857995908b')   
-  // fetch('http://api.openweathermap.org/data/2.5/weather?q=' + $(cityNameEl) + '&appid=' + $(apiKey))   
+  // fetch('http://api.openweathermap.org/data/2.5/weather?q=Pittsburgh&appid=a4fdb9683d6a1f4cdbbf60857995908b')   
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=' + cityNameEl + '&appid=' + apiKey)   
   .then(function(resp) { 
     return resp.json() 
   })
@@ -42,37 +33,59 @@ function cityWeather(cityNameEl) {
     getWeather(data);
   })
   .catch(function() {
-    
   });
 }
 
-window.onload = function() {
-  cityWeather();
-}
-
-
-
-
-
 function getWeather(weatherData) {
 	var fahrenheit = Math.round(((parseFloat(weatherData.main.temp)-273.15)*1.8)+32); 
+  console.log(weatherData);
   // var weatherIcon = document.querySelector('#icon');
-  // const {icon} = data.weather[0];
+  // const icon = data.weather[0];
   // var iconUrl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 	
-	document.getElementById('description').innerHTML = weatherData.weather[0].description;
-  // document.getElementById('weatherIcon').innerHTML = weatherIcon(iconUrl);
-	document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
-	document.getElementById('location').innerHTML = weatherData.name;
-  document.getElementById('wind').innerHTML += weatherData.wind.speed;
-  document.getElementById('humidity').innerHTML += weatherData.main.humidity;
+	$('#description').text(weatherData.weather[0].description);
+  // document.getElementById('weatherIcon').innerHTML = weatherData.weather.icon(iconUrl);
+	$('#temp').text(fahrenheit + ' &deg');
+	$('#location').text(weatherData.name);
+  $('#wind').text(weatherData.wind.speed);
+  $('#humidity').text(weatherData.main.humidity);
   // document.getElementById('uv').innerHTML = weatherData.uvi; 
   // not seeing UV index in console but it's supposed to be uvi
+  getForecastData(weatherData.name);
+
+}
+
+function getForecastData(cityName) {
+  let headers = new Headers();
+
+  return fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + apiKey, {
+    method: 'GET',
+    headers: headers
+  }).then(function(resp) { 
+    return resp.json() 
+  })
+  .then(function(data) {
+    console.log(data);
+    for (var i= 0; i < data.list.length; i++) {
+      if (data.list[i].dt_txt.indexOf('15:00:00') !== -1) {
+        console.log(data.list[i]);
+      }
+     
+    }
+  })
+  .catch(function() {
+  });
 }
 
 
 
-
+// //need to append to List of Searched Cities - not working!!
+// var printCities = function (name) {
+//   var listEl = $('<li>');
+//   var listDetail = name;
+//   listEl.addClass('list-group-item').text(listDetail);
+//   listEl.appendTo(cityListEl);
+// }
 
 
 
