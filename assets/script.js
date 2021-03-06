151b9c238ccd1a229f1c19e24a81d3e7ca219868
HRush <hrkoren@gmail.com>
2021-03-05 20:35:50 -0500

    got all api's working, updated html and css

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,9 @@ let cityInputEl = $('#city-search');
 let cityContainerEl = $('#cityContainer');
 let citySearchName = $('#city-search-name');
 let cityNameEl = $('.cityName');
-let searchBtn = $('.btn'); 
-let cityListEl = $('#city-list');
+let searchBtn = $('.btn');
+let cityListEl = $('.city-history');
+let searchHistory = [];
 const apiKey = 'a4fdb9683d6a1f4cdbbf60857995908b';
 
 function handleFormSubmit(event) {
@@ -16,34 +17,50 @@ function handleFormSubmit(event) {
     return;
   }
   cityWeather(searchInputEl);
+  localStorage.setItem('city-history', JSON.stringify(searchInputEl));
+
+  postSearchHistory();
+}
+
+function postSearchHistory(){
+  let storedCity = JSON.parse(localStorage.getItem('city-history'));
+  searchHistory = storedCity;
+
+  for (var i = 0; i < searchHistory.length; i++) {
+    // let dayDate = $('<div class="day_date">').text(data.list[i].dt_text);
+      let citySearchHist = $('<ul class="city-history">').addClass('id', 'list-group');
+      let searchHistoryList = $('<li>').addClass('id', 'list-group-item')
+       + searchHistoryList.text(searchHistory[i]);
+      cityListEl.append(citySearchHist);
+
+      citySearchHist.append(searchHistoryList);
+  }
 }
 
 searchFormEl.on('submit', handleFormSubmit);
 
-
-
 function cityWeather(cityNameEl) {
-     
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=' + cityNameEl + '&appid=' + apiKey)   
-  .then(function(resp) { 
-    return resp.json() 
-  })
-  .then(function(data) {
-    getWeather(data);
-  })
-  .catch(function() {
-  });
+
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=' + cityNameEl + '&appid=' + apiKey)
+    .then(function (resp) {
+      return resp.json()
+    })
+    .then(function (data) {
+      getWeather(data);
+    })
+    .catch(function () {
+    });
 }
 
 function getWeather(weatherData) {
-	var fahrenheit = Math.round(((parseFloat(weatherData.main.temp)-273.15)*1.8)+32); 
+  var fahrenheit = Math.round(((parseFloat(weatherData.main.temp) - 273.15) * 1.8) + 32);
   console.log(weatherData);
 
-	$('#description').text(weatherData.weather[0].description);
+  $('#description').text(weatherData.weather[0].description);
   $('#icon').html(`<img src='http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png'>`);
-	$('#temp').text(fahrenheit)
-    +($('.fa-fahrenheit'));
-	$('#location').text(weatherData.name);
+  $('#temp').text(fahrenheit)
+    + ($('.fa-fahrenheit'));
+  $('#location').text(weatherData.name);
   $('#wind').text(weatherData.wind.speed);
   $('#humidity').text(weatherData.main.humidity);
   // document.getElementById('uv').innerHTML = weatherData.uvi; 
@@ -53,30 +70,36 @@ function getWeather(weatherData) {
 }
 
 function getForecastData(cityName) {
- 
+
   return fetch('http://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + apiKey, {
     method: 'GET',
-    
-  }).then(function(resp) { 
-    return resp.json() 
+
+  }).then(function (resp) {
+    return resp.json()
   })
-  .then(function(data) {
-    console.log(data);
-    for (var i= 0; i < data.list.length; i++) {
-      if (data.list[i].dt_txt.indexOf('12:00:00') !== -1) {
-        console.log(data.list[i]);
+    .then(function (data) {
+      console.log(data);
+      for (var i = 0; i < data.list.length; i++) {
+        console.log(i);
+        if (data.list[i].dt_txt.indexOf('03:00:00') !== -1) {
+          // if (data.list[i].dt_txt.includes('03:00:00')) {
+          console.log(data.list[i]);
+          var fahrenheit = Math.round(((parseFloat(data.list[0].main.temp) - 273.15) * 1.8) + 32);
+
+          let dayContainer = $('<div class="day" + (i+1) + ">');
+          $('#five-day-box').append(dayContainer);
+
+          let dayDate = $('<div class="day_date">').text(data.list[i].dt_text);
+          let dayDesc = $('<div class="day_description">').text(data.list[i].weather.description);
+          let icon = $(`<img src='http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png'>`);
+          let dayTemp = $('<div class="day_temp">').text("Temp: " + fahrenheit);
+          let dayHumidity = $('<div class="day_humidity">').text("Humidity: " + data.list[i].main.humidity + "%");
+
+          dayContainer.append(dayDate, dayDesc, icon, dayTemp, dayHumidity);
       }
-      var fahrenheit = Math.round(((parseFloat(weatherData.main.temp)-273.15)*1.8)+32); 
-      
-      $('.day_date').text(list[0].dt_text);
-      $('.day_description').text(weatherData.weather[0].description);
-      $('#icon').html(`<img src='http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png'>`);
-      $('.day_temp').text(fahrenheit);
-      $('.day_humidity').text(weatherData.main.humidity);
     }
   })
 
-  .catch(function() {
-  });
-}
- 
+    .catch(function () {
+    });
+  }
